d15ade2f1ff0330a1bdcdf15d1ff184c6126270f
Adoggman <andrew.hanrahan@gmail.com>
2021-05-24 02:12:01 -0500

    SQL tables updated

--- sql/tables.sql
+++ sql/tables.sql
@@ -11,7 +11,43 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for funtown
+CREATE DATABASE IF NOT EXISTS `funtown` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `funtown`;
+
+-- Dumping structure for table funtown.characters
+CREATE TABLE IF NOT EXISTS `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_discord` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+
+-- Dumping structure for table funtown.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `steam` varchar(255) DEFAULT NULL,
+  `discord` varchar(255) DEFAULT NULL,
+  `is_admin` bit(1) NOT NULL DEFAULT b'0',
+  `is_dev` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `discord` (`discord`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table funtown.player_skins
+CREATE TABLE IF NOT EXISTS `player_skins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(128) NOT NULL,
+  `skin` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `identifier` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
