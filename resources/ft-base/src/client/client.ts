66ad48342ceb260f0685688458c1a994ce05cfca
Adoggman <andrew.hanrahan@gmail.com>
2021-05-31 15:35:48 -0500

    Dispatch moved to new file

--- resources/ft-base/src/client/client.ts
+++ resources/ft-base/src/client/client.ts
@@ -3,16 +3,6 @@ import Event from "../shared/events";
 import { currentChar, UpdateGamertag, SelectChar} from "./lib/char";
 import * as nui from "./lib/nuiLib";
 
-setTick(() => {
-	// Turn off emergency/police/ems dispatch every frame
-	for (let i = 0; i < 25; i++) {
-		EnableDispatchService(i, false);
-	}
-
-	// Turn off wanted level
-	ClearPlayerWantedLevel(PlayerId());
-});
-
 on(Event.playerSpawned, () => {
 	if (!currentChar()) {
 		nui.SendMessage("charSelect", "open", {forceChoice: true });
---
+++ resources/ft-base/src/client/dispatch.ts
@@ -0,0 +1,9 @@
+setTick(() => {
+	// Turn off emergency/police/ems dispatch every frame
+	for (let i = 0; i < 25; i++) {
+		EnableDispatchService(i, false);
+	}
+
+	// Turn off wanted level
+	ClearPlayerWantedLevel(PlayerId());
+});
