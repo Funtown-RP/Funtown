d401a407d1d28620736c25927830ffb1e31236a1
Adoggman <andrew.hanrahan@gmail.com>
2021-06-01 03:14:11 -0500

    Tabs on debug page

--- resources/ft-base/ui/src/apps/debug.tsx
+++ resources/ft-base/ui/src/apps/debug.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
-import { TableRow, TableCell, Paper, TableContainer, Table, TableHead, TableBody, Container, Button, Card } from '@material-ui/core';
+import { TableRow, TableCell, Paper, TableContainer, Table, TableHead, TableBody, Container, Button, Card, Tabs, Tab, Typography, Box } from '@material-ui/core';
 import { SendMessage } from "../lib/nui"
 import { item } from '../../../src/shared/interfaces';
 import CloseIcon from '@material-ui/icons/Close';
 
 export interface DebugScreenProps {
 	close: () => void;
-} 
+}
 
-export class DebugScreen extends React.Component<DebugScreenProps, any> {
+export interface DebugScreenState {
+	value: number;
+	items: item[];
+}
 
-	items: item[] = [];
+export class DebugScreen extends React.Component<DebugScreenProps, DebugScreenState> {
 
 	constructor(props: DebugScreenProps) {
 		super(props)
+		this.state = { value: 0, items: [] };
 		this.getItems();
+
+		this.getItems = this.getItems.bind(this);
+		this.itemDefs = this.itemDefs.bind(this);
 	}
 
 	getItems() {
 		SendMessage("debug", "items").then((response) => {
 			if (!!response) {
-				this.items = response;
-				this.forceUpdate();
+				this.setState({value: this.state.value, items: response});
 			}
 		});
 	}
 
-	render() {
-		return <Container maxWidth="md" >
-			<Paper style={{padding: "32px", margin: "16px"}} >
-			<Button variant="contained" color="secondary" endIcon={<CloseIcon />} onClick={() => this.props.close()} >Close</Button>
-			<Button variant="contained" color="primary" onClick={() => this.getItems()} >Refresh</Button>
-			<TableContainer component={Card} >
+	itemDefs() {
+		return <TableContainer component={Card} >
 			<Table>
 				<TableHead>
 					<TableRow>
@@ -42,7 +44,7 @@ export class DebugScreen extends React.Component<DebugScreenProps, any> {
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{Array.isArray(this.items) && this.items.map((item: item) => (
+					{Array.isArray(this.state?.items) && this.state?.items?.map((item: item) => (
 						<TableRow key={item.key}>
 							<TableCell>{item.key}</TableCell>
 							<TableCell>{item.name}</TableCell>
@@ -53,7 +55,21 @@ export class DebugScreen extends React.Component<DebugScreenProps, any> {
 				</TableBody>
 			</Table>
 		</TableContainer>
-		</Paper>
-	</Container>
+	}
+
+	render() {
+		return <Container maxWidth="md" >
+			<Paper style={{padding: "32px", margin: "16px"}} >
+				<Button variant="contained" color="secondary" endIcon={<CloseIcon />} onClick={() => this.props.close()} >Close</Button>
+				<Tabs centered value={this.state.value} style={{ marginBottom: "24px" }} onChange={(event: object, value: number) => {
+					this.setState({value: value});
+				}}>
+					<Tab label="Item Definitions" />
+					<Tab label="Test" />
+				</Tabs>
+				{this.state.value === 0 && <this.itemDefs />}
+				{this.state.value === 1 && <Typography variant="h5" component="h2" style={{display: 'flex', justifyContent: 'center'}}>Test tab</Typography>}
+			</Paper>
+		</Container>
 	}
 }
