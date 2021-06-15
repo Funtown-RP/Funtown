bbae4b416ea80a100571482b05f3647545869371
Adoggman <andrew.hanrahan@gmail.com>
2021-06-14 21:20:39 -0500

    Add heading to spawn

--- resources/[fivem]/[gamemodes]/[maps]/fivem-map-hipster/map.lua
+++ resources/[fivem]/[gamemodes]/[maps]/fivem-map-hipster/map.lua
@@ -1,5 +1,5 @@
 vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
 
-spawnpoint 'mp_m_freemode_01' { x = 313.31, y = -232.97, z = 53.99 }
+spawnpoint 'mp_m_freemode_01' { x = 313.31, y = -232.97, z = 53.99, heading = 158.00 }
 
 --
